8e7c54a02800b47835945d9746ea8b5d227ffcc5
Alphaeus Mote <Alphaeus.Mote@gmail.com>
2025-05-02 15:43:03 -0400

    Update version to 2025.05.02.1542

--- src/VersionInfo.cs
+++ src/VersionInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 
 // Version information for the assembly
 // This file will be updated by inject-version.ps1 script
-[assembly: AssemblyVersion("2025.05.02.1228")]
-[assembly: AssemblyFileVersion("2025.05.02.1228")]
-[assembly: AssemblyInformationalVersion("2025.05.02.1228")]
+[assembly: AssemblyVersion("2025.05.02.1542")]
+[assembly: AssemblyFileVersion("2025.05.02.1542")]
+[assembly: AssemblyInformationalVersion("2025.05.02.1542")]
